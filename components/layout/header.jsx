e6576d5bb9b8d3f14c219e52b4b3e45368c4cc03
codanMartin <codanmartin.25@gmail.com>
2024-09-10 19:33:09 +0300

    fix: link in nav for homepage

--- components/layout/header.jsx
+++ components/layout/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <header className="flex sticky top-0 z-50 bg-white w-full h-16 border-b border-gray-300 shadow">
             <div className="flex items-center h-full w-full justify-between gap-4">
-                <Link href="/public" className="pl-[10.5px] focus-within:outline-none">
+                <Link href="/" className="pl-[10.5px] focus-within:outline-none">
                     <LogoWithTitle className={"flex w-full"}/>
                 </Link>
                 <Link href="/formular-finantare" className="h-full group flex items-center bg-eu-blue text-white sm:text-nowrap sm:text-lg sm:font-bold">
